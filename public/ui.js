30a93439da846f67156b32ac5048f1af55de63f4
UX팀-김상민 <hobak2k@hyundaihmall.com>
2019-02-12 11:04:42 +0900

    #UI

--- public/ui.js
+++ public/ui.js
@@ -38,20 +38,48 @@ $(function() {
       var reviews = '';
       for (var i = 0; i < data.length; i++) {
         var review = '';
-        reviews += `<li><div class="_review"><div class="_star"><img src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678064-star-512.png" alt="3점">
-        <img src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678064-star-512.png" alt="3점">
-        <img src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678064-star-512.png" alt="3점"><span>점수</span> : 5</div><div class="_subject">제목이 제목이지 제목이 제목이겠냐</div><div class="_content">좋은 상품을 소개해주셔서 늘 애용하고있읍니다~~ 고마워요~ 좋은 상품을 소개해주셔서 늘 애용하고있읍니다~~ 고마워요~ 좋은 상품을 소개해주셔서 늘 애용하고있읍니다~~ 고마워요~ 좋은 상품을 소개해주셔서 늘 애용하고있읍니다~~ 고마워요~ 좋은 상품을 소개해주셔서 늘 애용하고있읍니다~~ 고마워요~ 좋은 상품을 소개해주셔서 늘 애용하고있읍니다~~ 고마워요~ 좋은 상품을 소개해주셔서 늘 애용하고있읍니다~~ 고마워요~</div><div class="_comment"><img src="https://cdn0.iconfinder.com/data/icons/faticons-2/31/comment21-512.png" alt=""></div><div class="_info"><span>아이디 : </span> hyundaihmall / <span>날짜 : </span>2019. 2. 9</div></div></li>`;
-        // review += '<ul>';
-        // review += '<li><span>운영체제</span> : ' + data[i].os + '</li>';
-        // review += '<li><span>유저명</span> : ' + data[i].review.userName + '</li>';
-        // review += '<li><span>날짜</span> : ' + data[i].review.date + '</li>';
-        // review += '<li><span>점수</span> : ' + data[i].review.score + '</li>';
-        // review += '<li><span>제목</span> : ' + data[i].review.title + '</li>';
-        // review += '<li><span>내용</span> : ' + data[i].review.text + '</li>';
-        // review += '<li><span>답글날짜</span> : ' + data[i].review.replyDate + '</li>';
-        // review += '<li><span>답글내용</span> : ' + data[i].review.replyText + '</li>';
-        // review += '</ul>';
-        // reviews += '<li>' + review + '</li>';
+          review += '<div class="_review">';
+            // 제목
+            if(data[i].review.title != ""){
+                review += '<div class="_subject">' + data[i].review.title + '</div>';
+            }
+
+            // 내용
+            review += '<div class="_content">' + data[i].review.text + '</div>';
+
+            // 사용자, 별점, 등록일
+            review += '<div class="_info">';
+                review += '<ul>';
+                review += '<li class="_user">';
+                    review += '<img src="'+ data[i].review.userImage +'" alt="">';
+
+                    if(data[i].review.userName != ""){
+                        review += '<span>' + data[i].review.userName + '</span>';
+                    }
+                review += '</li>';
+                review += '<li class="_rate">';
+                    review += '<img src="https://cdn3.iconfinder.com/data/icons/flat-actions-icons-9/792/Star_Gold-512.png" alt="">';
+                    review += '<span>' + data[i].review.score + '</span>';
+                review += '</li>';
+                review += '<li class="_date">';
+                    review += '<span>' + data[i].review.date + '</span>';
+                review += '</li>';
+            review += '</div>';
+          review += '</div>';
+
+          // 홈쇼핑 답변
+          if(data[i].review.replyDate != null){
+              review += '<div class="_comment">';
+                    review += '<div class="_icon">';
+                        review += '<img src="https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/support-512.png" alt="">';
+                        review += '<span>'+ data[i].review.replyDate +'</span>';
+                    review += '</div>';
+                    review += '<div class="_content">' + data[i].review.replyText + '</div>';
+              review += '</div>';
+          }
+
+          reviews += '<li>' + review + '</li>';
+
       }
       $('.reviews.android').append(reviews);
     },
